4c5001b8336932c1136f6b7a0e962353fa07d057
Nicolas Payot <nicolas.payot@gmail.com>
2017-12-26 22:17:37 +0100

    Update layout

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,16 +1,20 @@
 <template>
   <article class="media">
-    <figure class="media-left">
-      <img :src="poster">
-    </figure>
-    <div class="media-content">
-      <div class="content">
-        <strong>{{ title }}</strong>
-        <br>
-        <small>{{ subtitle }}</small>
-        <p class="abstract">{{ abstract }}</p>
+    <div class="columns is-variable">
+      <div class="column is-one-quarter">
+        <img :src="poster">
+      </div>
+      <div class="column">
+        <div class="media-content">
+          <div class="content">
+            <strong>{{ title }}</strong>
+            <br>
+            <small>{{ subtitle }}</small>
+            <p class="abstract">{{ abstract }}</p>
 
-        <slot></slot>
+            <slot></slot>
+          </div>
+        </div>
       </div>
     </div>
   </article>
